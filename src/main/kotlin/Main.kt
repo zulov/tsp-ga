b787f0c06d98579ffafc9fe8aa5e8c632bb6aee2
Tomasz Gajda <zylov92@gmail.com>
2025-05-21 15:47:48 +0200

    clean

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -12,39 +12,40 @@ import kotlin.system.measureTimeMillis
 val pointRepository = PointRepository()
 
 val STEPS_NO = listOf(1000)
-val POPULATION_SIZE = listOf(10_000)
+val POPULATION_SIZE = listOf(100_000)
 val SURVIVOR_RATE = listOf(0.8F)
 val MUTATION_CHANCE = listOf(0.3F)
 val GRANDFATHER_RATE = listOf(0.9f)
 val NN_RATE = listOf(0.1f)
 fun main() {
-    println(LocalTime.now().format(timeFormatter))
+    printTime("Start: ")
     pointRepository.load("xit1083")
 
-    val results: MutableMap<ResultKey, Int> = mutableMapOf()
+    val results: MutableList<Pair<ResultKey, Int>> = mutableListOf()
     val resolver = Resolver(pointRepository)
     prepareParameters()
         .forEach { p ->
             val processTime = measureTimeMillis {
                 val (_, distance) = resolver.init(p).process()
-                results[p] = distance
+                results.add(p to distance)
                 println("$p = $distance")
             }
             println("Time process: ${processTime / 1000}s, per step: ${processTime / p.steps} ms")
         }
 
     println("Results:")
-    results.entries.sortedBy { it.value }.forEach {
-        println("${it.key} -> ${it.value}")
-    }
+    results.sortedBy { it.second }.forEach { println("${it.first} -> ${it.second}") }
     println("Grouped:")
     groupAndPrintResults(results, "survivor") { x -> x.survivor.toString() }
     groupAndPrintResults(results, "mutation") { x -> x.mutation.toString() }
     groupAndPrintResults(results, "population") { x -> x.population.toString() }
     groupAndPrintResults(results, "grandfather") { x -> x.grandfather.toString() }
     groupAndPrintResults(results, "initNnRate") { x -> x.initNnRate.toString() }
-    println(LocalTime.now().format(timeFormatter))
+    printTime("End: ")
 }
+
+private fun printTime(prefix: String): Unit =
+    println(prefix + LocalTime.now().format(timeFormatter))
 
 private fun prepareParameters(): List<ResultKey> = STEPS_NO.flatMap { steps ->
     POPULATION_SIZE.flatMap { population ->
@@ -60,8 +61,8 @@ private fun prepareParameters(): List<ResultKey> = STEPS_NO.flatMap { steps ->
     }
 }
 
-fun groupAndPrintResults(results: Map<ResultKey, Int>, name: String, getter: (ResultKey) -> String) {
-    val lines = results.entries.map { getter(it.key) to it.value }
+fun groupAndPrintResults(results: List<Pair<ResultKey, Int>>, name: String, getter: (ResultKey) -> String) {
+    val lines = results.map { getter(it.first) to it.second }
         .groupBy { it.first }
         .map { it.key to it.value.map { it.second }.average() }
         .sortedBy { it.second }
