139bafe6b5f9aed719352e2a48c536005a766ee4
Tomasz Gajda <zulov92@gmail.com>
2026-04-14 13:05:19 +0200

    fix two opt

--- src/main/kotlin/algo/TwoOpt.kt
+++ src/main/kotlin/algo/TwoOpt.kt
@@ -6,24 +6,25 @@ class TwoOpt(
 
     // ocenianie poprawy tylko na fragmencie ma sens jeżeli koszt w obie strony jest taki sam
     fun improve(path: Path, limit: Int = 5): Path {
-        var c = 0
+        var iteration = 0
         var improved = true
         val n = path.size
-        while (improved && c < limit) {
+        while (improved && iteration < limit) {
             improved = false
-            for (i in 1 until n - 1) {
-                val a = path[i - 1]
+            for (i in 0 until n - 1) {
+                val a = path[(i - 1 + n) % n]
                 for (j in i + 2 until n) {
+                    if (i == 0 && j == n - 1) continue
                     val b = path[i]
                     val c = path[j - 1]
-                    val d = path[j]
+                    val d = path[j % n]
                     if (costService.deltaBelowZero(a, b, c, d)) {
                         path.reverse(i, j)
                         improved = true
                     }
                 }
             }
-            c++
+            iteration++
         }
 
         return path
